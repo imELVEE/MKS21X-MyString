18adf1667e1df73ce61468d98add95e0331e0833
Alvi Aziz <aaziz00@stuy.edu>
2018-11-26 01:48:43 -0500

    Provided correct error messages and caught appropriate errors

--- MyString.java
+++ MyString.java
@@ -8,28 +8,61 @@ public class MyString implements CharSequence{
     }
   }
   public String toString(){
-    String ans = "[" + data[0];
-    for (int i = 1 ; i < data.length ; i++){
-      ans += ", " + data[i];
+    String ans = "";
+    for (int i = 0; i < data.length ; i++){
+      ans += data[i];
     }
-    return ans + "]";
+    return ans;
   }
 
 
   public char charAt(int index){
-    return data[index];
+    try{
+      //Stops charAt if there's no error because return exits charAt
+      return data[index];
+    }
+    catch(IndexOutOfBoundsException e){
+      //Tells user what user did wrong
+      System.out.print("Index provided is invalid: Index was " + index + ".");
+    }
+    //return garbage answer
+    return ' ';
   }
+
   public int length(){
     return data.length;
   }
+
   public CharSequence subSequence(int start, int end){
-    String subdata = "";
-    for (int i = 0 ; i < data.length ; i++){
-      if (i >= start && i < end){
-        subdata += data[i];
+    try{
+      //Check to see if the end is invalid, if only end is negative or
+      //smaller than start
+      char endTest = data[end-start];
+
+      //Create the answer
+      String subdata = "";
+      //Loops through the data inbetween start and end
+      //Checks for: if the start is negative or start end > length
+      for (int i = start ; i < end ; i++){
+          subdata += data[i];
       }
+      //return the subdata and exit the program
+      return subdata;
     }
-    return subdata;
+    catch(IndexOutOfBoundsException e){
+      //Tells user what user did wrong
+      System.out.print("Start and end are invalid:");
+      if (start < 0)
+        System.out.print(" Start was " + start + ".");
+      if (end < 0)
+        System.out.print(" End was " + end + ".");
+      if (end < start)
+        System.out.print(" End was less than start.");
+      if (end > data.length)
+        System.out.print(" End was greater than length of MyString.");
+    }
+    //return garbage value if it error occurs
+    return "";
   }
 
 }
--- mydriver.java
+++ mydriver.java
@@ -2,5 +2,13 @@ public class mydriver{
   public static void main(String[] args){
     MyString constructorTest = new MyString("test");
     System.out.println(constructorTest);
+    System.out.println(constructorTest.charAt(0));
+    System.out.println(constructorTest.charAt(-1));
+    System.out.println(constructorTest.subSequence(0,2));
+    System.out.println(constructorTest.subSequence(0,5));
+    System.out.println(constructorTest.subSequence(-1,2));
+    System.out.println(constructorTest.subSequence(0,-1));
+    System.out.println(constructorTest.subSequence(2,1));
+    System.out.println(constructorTest.subSequence(-2,-1));
   }
 }
